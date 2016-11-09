d2f806993d1917e83628b5929c21e5567768a66b
=uday Singh <=usudaysingh.39@gmail.com>
2016-11-09 15:02:20 +0530

    insertion and bubble sort

--- sorting/sort.py
+++ sorting/sort.py
@@ -1,2 +1,29 @@
-print "Hi, Welcome to sorting file."
+def bubblesort():
+	unsorted = [1,90,343,54,11,43,23,76,54,93,3,01,10]
+	for i in range( len( unsorted ) ):
+		for k in range( len( unsorted ) - 1, i, -1 ):
+			if(unsorted[k]<unsorted[i]):
+				temp = unsorted[k]
+				unsorted[k]=unsorted[i]
+				unsorted[i]=temp
 
+	print unsorted
+
+def insertionsort():
+	unsorted = [1,90,343,54,11,43,23,76,54,93,3,01,10]
+	for i in range(len(unsorted)):
+		for k in range(len(unsorted[:i])):
+			if(unsorted[i]<unsorted[k]):
+				temp = unsorted[k]
+				unsorted[k]=unsorted[i]
+				unsorted[i]=temp
+	print unsorted
+
+def selectionsort():
+	unsorted = [1,90,343,54,11,43,23,76,54,93,3,01,10]
+	for i in range(len(unsorted)):
+		for k in range(len(unsorted)):
+			
+
+bubblesort()
+insertionsort()
